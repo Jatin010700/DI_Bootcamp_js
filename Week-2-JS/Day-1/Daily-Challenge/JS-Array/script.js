94e5c1c7c97927e5d0e7b2af977f1a8b20ac24e8
Jatin010700 <130746825+Jatin010700@users.noreply.github.com>
2023-04-24 15:10:54 +0400

    week2 JS daily challenge

---
+++ Week-2-JS/Day-1/Daily-Challenge/JS-Array/script.js
@@ -0,0 +1,17 @@
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+console.log(fruits);
+fruits.splice(0,1);
+console.log(fruits)
+fruits.sort();
+console.log(fruits);
+fruits.push("kiwis");
+console.log(fruits);
+fruits.splice(0,1);
+console.log(fruits);
+fruits.reverse();
+console.log(fruits);
+
+//----------------------------
+
+let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+console.log("oranges");
---
+++ Week-2-JS/Day-1/Normal-Exercise/Exercise-1-JS/script.js
@@ -0,0 +1,22 @@
+let addressNumber= 123456789;
+let addressStreet="I live in Port-Louis,";
+let country = "Mauritius";
+
+console.log(addressNumber);
+console.log(addressStreet);
+console.log(country);
+console.log("I live at" + " " + addressNumber,addressStreet,country);
+
+let birthYear = 2000;
+let futureYear = 2100;
+
+console.log("I will be" + " " + (2100-2000))
+
+let pets = (['cat','dog','fish','rabbit','cow'])
+console.log(pets)
+let array = pets [1]
+console.log(array)
+pets.push("horse")
+console.log(pets)
+pets.splice(3,1)
+console.log(pets)
