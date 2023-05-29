93cbbc2dae1c76b9ee358a85d0b67b90c23a1a4a
Jatin010700 <130746825+Jatin010700@users.noreply.github.com>
2023-05-29 11:50:42 +0400

    updated

---
+++ Week-7-Node.js/Day-1/Test-Exercise/calculator.js
@@ -0,0 +1,8 @@
+let operator = require('./operators.js');
+
+// import {addOperator,divideOperator} from 'operators.js'
+
+operator.sum2num(5+2)
+operator.divide2num(10/2)
+
+console.log(operator)
---
+++ Week-7-Node.js/Day-1/Test-Exercise/operators.js
@@ -0,0 +1,10 @@
+const addOperator = (x,y) => {
+    return x + y
+}
+const divideOperator = (x,y) => {
+    return x / y
+}
+module.exports = {
+    sum2num: addOperator,
+    divide2num: divideOperator
+}
