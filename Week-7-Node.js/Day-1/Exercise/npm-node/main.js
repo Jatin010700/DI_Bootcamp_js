ed803a7fbbd51ff9fe57cae480f20c6fb6041e9c
Jatin010700 <130746825+Jatin010700@users.noreply.github.com>
2023-05-29 14:51:51 +0400

    ex

---
+++ Week-7-Node.js/Day-1/Exercise/npm-node/main.js
@@ -0,0 +1,15 @@
+// const a = 5;
+// const b = 10;
+
+// console.log("Hello you are", a+b)
+
+//----part 3----
+
+const a = 5;
+const b = 10;
+let x;
+const c = 6;
+x = a + b;
+
+console.log("Hello you are", a+b)
+console.log(x+c)
