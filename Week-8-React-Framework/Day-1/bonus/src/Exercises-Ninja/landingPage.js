f72b4f33ecbff0290e1695a4ae405756eebb06fa
Jatin010700 <130746825+Jatin010700@users.noreply.github.com>
2023-06-07 12:47:43 +0400

    exercise ninja completed

--- Week-8-React-Framework/Day-1/bonus/src/Exercises-Ninja/landingPage.js
+++ Week-8-React-Framework/Day-1/bonus/src/Exercises-Ninja/landingPage.js
@@ -1,11 +1,124 @@
 import React from 'react';
+import './landing-page.css';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLandmark , faBuilding, faGlobeAfrica} from '@fortawesome/free-solid-svg-icons';
 
 function Landingpage() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
+  return(
+        <div id='body'>
+            <Header/>
+            <Card 
+                className='section'
+                icon={faBuilding}
+                title='About the Company' 
+                description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                        aliquip ex ea commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                        pariatur.'
+            />
+
+            <Card 
+                className='section bg-grey '
+                icon = {faGlobeAfrica}
+                title='Our Values'
+                description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                        aliquip ex ea commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                        pariatur.'
+            />
+
+            <Card 
+                className='section'
+                icon={faLandmark}
+                title='Our Mission' 
+                description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                        aliquip ex ea commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                        pariatur.'
+            />
+            <ContactContainer/>
+        </div>
+    );
 }
 
+const Header = () =>{
+    return(
+        <div className='header'>
+            <span className='header-title'>
+                Company
+            </span>
+            <br/>
+            <span className="header-text">
+                We specialise in something ...
+            </span>
+        </div>
+    );
+}
+
+
+const Card = (props) =>{
+    return(
+        <div className={props.className} >
+      {props.icon && <FontAwesomeIcon icon={props.icon} />}
+
+            <div className="big-div">
+                <span className='div-title'>
+                    {props.title}
+                </span>
+                <br/>
+                <span>
+                    {props.description}
+                </span>
+            </div>
+        </div>
+    )
+}
+
+
+
+const ContactContainer = () => {
+    return(
+        <div className='contact-container bg-grey'>
+            <span className="div-title">Contact us</span>
+            <div className='contact-form'>
+                <div id='sect1'>
+                    <span>Contact us and we will get back to you within 24 hours.</span>
+                    <span>
+                        <i className="fas fa-map-marker-alt"></i>
+
+                        Company Name
+                    </span>
+                    <span>
+                        <i className="fas fa-mobile-alt"></i>
+
+                        +256 778 800 900
+                    </span>
+                    <span>
+                        <i className="fas fa-envelope"></i>
+
+                        company.gmail.com
+                    </span>
+                </div>
+                    
+                <div id='sect2'>
+                    <span>
+                        Contact
+                    </span>
+
+                    <input type="text" placeholder="email address" className="input-field"/>
+                    <textarea name="" id="" cols="30" rows="10" placeholder="comment"></textarea>
+                    <button className="contact-btn">Send</button>
+                </div>
+            </div>
+        </div>
+    );
+
+}
 export default Landingpage;
